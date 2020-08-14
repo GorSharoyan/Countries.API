b42274e5cd6204fbd4b70cfeacd7d21c3d48e7ff
Gor <gor.sharoyan95@gmail.com>
2020-08-15 03:22:55 +0400

    finalized index.html design pattern

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 let login =document.querySelector('#login');
-let password=documen.querySelector('#password')
+let password=document.querySelector('#password');
+let submitButton=document.querySelector('#submit-button');
+let loginPageBody=document.querySelector('.body');
+
+submitButton.addEventListener('click',(event)=>{
+    event.preventDefault();
+    console.log('Yurik Jaja')
+    if((login.value==='admin')&&(password.value==='admin')){
+     
+    }else{
+        login.value='';
+        password.value='';
+        let errorMessage=docuemnt.createElement('div');
+        let messageText='Wrong login or password !!! Try agian!!!';
+        errorMessage.innerHTML=messageText;
+        loginPageBody.append(errorMessage);
+    }
+})
