f3f5bf6a6aa49d338828bf417c63b3abfaecc684
Gor <gor.sharoyan95@gmail.com>
2020-08-15 16:56:22 +0400

    added add/remove buttons to countries list

--- countries.js
+++ countries.js
@@ -1,14 +1,46 @@
 let searchInput =document.querySelector('#search-input');
 let searchBtn=document.querySelector('#search-button');
+let tbody=document.querySelector('tbody');
 
 searchBtn.addEventListener('click',(event)=>{
     event.preventDefault();
-    searchInput.value='';
-    getCountriesInfo();
+    
+    // if(searchInput.value){
+    //     getError(searchInput.value)
+    //     searchInput.value='';
+    // }else{
+        getCountriesInfo(searchInput.value);
+        searchInput.value='';
+    // }
 })
 
-async function getCountriesInfo(){
-    let response=await fetch ('https://restcountries.eu/rest/v2/all');
-    let countries=await response.json();
-    console.log(countries);
+async function getCountriesInfo(name){
+    let response=await fetch (`https://restcountries.eu/rest/v2/name/${name}`);
+    let country=await response.json();
+    console.log(country);
+    createTableRow(country)
 }
+function createTableRow(country){
+    let tr=document.createElement('tr');
+    let favBtn=document.createElement('button');
+    let removeBtn=document.createElement('button');
+    removeBtn.innerText='remove';
+    favBtn.innerText='add';
+
+    let countryName=country.borders;
+    tr.append(countryName);
+    tr.append(favBtn);
+    tr.append(removeBtn);
+    tbody.append(tr);
+    
+}
+// function getError(value){
+//     if(Number(value.trim())==='NaN'){
+//         let errorMessage=document.createElement('div');
+//         div.classList='error-message';
+//         let errorText=document.createElement('p');
+//         errorText.innerText='Invail input value .Input value must be a string!!!';
+//         errorMessage.append(errorText);
+//         tbody.append(errorMessage);
+//     }
+// }
