c95a57a8271e5ea9cf6a873eb51ce4d369dddad0
Gor <gor.sharoyan95@gmail.com>
2020-08-15 15:25:42 +0400

    created countries.js,updated countries.html

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-let login =document.querySelector('#login');
-let password=document.querySelector('#password');
-let submitButton=document.querySelector('#submit-button');
-let loginPageBody=document.querySelector('.body');
+let login = document.querySelector("#login");
+let password = document.querySelector("#password");
+let submitButton = document.querySelector("#submit-button");
+let body = document.querySelector("body");
 
-submitButton.addEventListener('click',(event)=>{
-    event.preventDefault();
-    console.log('Yurik Jaja')
-    if((login.value==='admin')&&(password.value==='admin')){
-     
-    }else{
-        login.value='';
-        password.value='';
-        let errorMessage=docuemnt.createElement('div');
-        let messageText='Wrong login or password !!! Try agian!!!';
-        errorMessage.innerHTML=messageText;
-        loginPageBody.append(errorMessage);
-    }
-})
+localStorage.setItem("login", "Gor");
+localStorage.setItem("password", 123);
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (passValidation(login, password)) {
+    console.log("passed!!!");
+  } else {
+    getError()
+  }
+});
+
+function getError() {
+    login.value = "";
+    password.value = "";
+ 
+    let errorMessage=document.createElement('div');
+    errorMessage.classList='error-message';
+    let errorText=document.createElement('h1');
+    errorText.innerText='Invaild Login or password !!! Valid properties in readme file';
+    errorMessage.append(errorText);
+    body.append(errorMessage);
+}
+
+function passValidation(login, password) {
+  
+  if (
+    login.value === localStorage.getItem("login") &&
+    password.value === localStorage.getItem("password")
+  ) {
+    login.value = "";
+    password.value = "";
+    return true;
+  }
+}
