ff9bce7146627c219d5702b5e6fcd5449c25a0a9
Gor <gor.sharoyan95@gmail.com>
2020-08-18 00:51:25 +0400

    added index.css/mains/

--- countries.js
+++ countries.js
@@ -28,24 +28,21 @@ const [span, div, tr, td, button, tbody,img] = [
 
 
 getCountriesInfo();
+
 searchBtn.addEventListener('click',(event)=>{
     event.preventDefault();
     
-    // if(searchInput.value){
-    //     getError(searchInput.value)
-    //     searchInput.value='';
-    // }else{
-        getCountriesInfo(searchInput.value);
+         getCountriesInfo(searchInput.value);
         searchInput.value='';
-    // }
-})
+});
 
 async function getCountriesInfo(){
     let response=await fetch (`https://restcountries.eu/rest/v2/all`);
     let countries=await response.json();
     console.log(countries);
     createTableRow(countries)
-}
+};
+
 function createTableRow(countries){
 
     const tableBody = tbody({}, countries.map(country =>
@@ -82,39 +79,11 @@ function createTableRow(countries){
     }
 }
        
-   
-
-    function removeFromFavourites (country) {
+   function removeFromFavourites (country) {
         let favourites = JSON.parse(localStorage.getItem('favourites'));
         if(favourites.some(c => c.name === country.name)){
             const newFavs = favourites.filter(c => c.name !== country.name);
            localStorage.setItem('favourites',JSON.stringify(newFavs));
         }
     }
-    // ])));
-    // countries.forEach( country => {
-    // let tr=document.createElement('tr');
-    // let td=document.createElement('td');
 
-    // let favBtn=document.createElement('button');
-    // let removeBtn=document.createElement('button');
-    // removeBtn.innerText='remove';
-    // favBtn.innerText='add';
-
-    // let countryName=country.name;
-    // // tr.append(countryName);
-    // // tr.append(favBtn);
-    // // tr.append(removeBtn);
-    // // tbody.append(tr);
-//     // })
-// }
-// function getError(value){
-//     if(Number(value.trim())==='NaN'){
-//         let errorMessage=document.createElement('div');
-//         div.classList='error-message';
-//         let errorText=document.createElement('p');
-//         errorText.innerText='Invail input value .Input value must be a string!!!';
-//         errorMessage.append(errorText);
-//         tbody.append(errorMessage);
-//     }
-// }
--- script.js
+++ script.js
@@ -3,33 +3,31 @@ let password = document.querySelector("#password");
 let submitButton = document.querySelector("#submit-button");
 let body = document.querySelector("body");
 
-
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
 
   if (isValid(login, password)) {
-    console.log("passed!!!");
     login.value = "";
     password.value = "";
-    window.location.href='./countries.html';
+    window.location.href = "./countries.html";
   } else {
-    getError()
+    getError();
   }
 });
 
 function getError() {
-    login.value = "";
-    password.value = "";
- 
-    let errorMessage=document.createElement('div');
-    errorMessage.classList='error-message';
-    let errorText=document.createElement('h1');
-    errorText.innerText='Invaild Login or password !!! Valid properties in readme file';
-    errorMessage.append(errorText);
-    body.append(errorMessage);
+  login.value = "";
+  password.value = "";
+
+  let errorMessage = document.createElement("div");
+  errorMessage.classList = "error-message";
+  let errorText = document.createElement("h1");
+  errorText.innerText =
+    "Invaild Login or password !!! Valid properties in readme file";
+  errorMessage.append(errorText);
+  body.append(errorMessage);
 }
 
 function isValid(login, password) {
-    
-    return login.value.length > 5 && password.value.length > 5;
+  return login.value.length > 5 && password.value.length > 5;
 }
