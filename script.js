d45dd0f24235e232cf3fb1b8d8cc3869c5d37835
Gor <gor.sharoyan95@gmail.com>
2020-08-19 03:28:15 +0400

    worked on lognPage UI/fixed error message bug

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let login = document.querySelector("#login");
 let password = document.querySelector("#password");
 let submitButton = document.querySelector("#submit-button");
-let body = document.querySelector("body");
+let errorMessage = document.querySelector(".error-message");
+
 
 submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
-
+  
   if (isValid(login, password)) {
-    login.value = "";
-    password.value = "";
-    window.location.href = "./countries.html";
+     window.location.href = "./countries.html";
   } else {
-    getError();
+       errorMessage.innerHTML='';
+      getError();
   }
 });
 
@@ -19,13 +18,10 @@ function getError() {
   login.value = "";
   password.value = "";
 
-  let errorMessage = document.createElement("div");
-  errorMessage.classList = "error-message";
-  let errorText = document.createElement("h1");
-  errorText.innerText =
-    "Invaild Login or password !!! Valid properties in readme file";
+  let errorText = document.createElement("h3");
+  errorText.innerText = "Invaild Login or password !!!";
   errorMessage.append(errorText);
-  body.append(errorMessage);
+
 }
 
 function isValid(login, password) {
---
+++ tods.md
@@ -0,0 +1,9 @@
+//Fix the problem with the error window
+//add JS docs in some places 
+//add DOM helpers 
+//write ode for search Input arrea 
+//Create Pagination|
+<!-- //Finish login pages UI/UX -->
+<!-- //apply approprpiate backgorund image  -->
+<!-- //finish login pages html tags part -->
+//add metadata 
